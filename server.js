9c23abb244474993597f79dc3974ad6fd0718b80
Jin KiHwan <eljsh95@naver.com>
2024-01-28 16:39:23 +0900

    몽고디비 호스팅 + 세팅

--- server.js
+++ server.js
@@ -2,6 +2,9 @@
 const express = require('express');
 const app = express();
 
+/* 스타일경로 등록 */
+app.use(express.static(__dirname + '/public'));
+
 //포트란?
 //웹서비스를 이용하는 것 = 타인 컴퓨터에 접속하는 것과 동일
 //평소에는 접속 불가, Port는 타인이 접속할 수 있게 만드는 것이다.
@@ -24,3 +27,6 @@ app.get('/about', (요청, 응답) => {
 app.get('/shop', (요청, 응답) => {
   응답.send('쇼핑페이지입니다');
 });
+
+/*node라이브러리  npm install -g nodemon */
+/* nodemon server.js <-터미널 입력 */
